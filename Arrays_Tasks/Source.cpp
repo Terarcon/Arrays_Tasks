3a40d8cf36db5a3b33685e32feaee4d2c5707a50
User <User@TERARCON>
2023-04-07 20:05:32 +0800

    shiftRight

--- Arrays_Tasks/Source.cpp
+++ Arrays_Tasks/Source.cpp
@@ -61,6 +61,7 @@ void shiftLeft(int array[ROWS][COLS], const int ROWS, const int COLS, int number
 void shiftRight(int array[], const int n, int number_of_shifts);
 void shiftRight(double array[], const int n, int number_of_shifts);
 void shiftRight(char array[], const int n, int number_of_shifts);
+void shiftRight(int array[ROWS][COLS], const int ROWS, const int COLS, int number_of_shifts);
 
 void UniqueRand(int array[], const int n);
 void UniqueRand(double array[], const int n);
@@ -184,6 +185,8 @@ void main()
 	int number_of_shifts;
 	cout << "Enter number of shifts: "; cin >> number_of_shifts;
 	shiftLeft(i_arr_2, ROWS, COLS, number_of_shifts);
+	Print(i_arr_2, ROWS, COLS);
+	shiftRight(i_arr_2, ROWS, COLS, number_of_shifts);
 	Print(i_arr_2, ROWS, COLS);
 }
 
@@ -672,7 +675,14 @@ void shiftLeft(int array[ROWS][COLS], const int ROWS, const int COLS, int number
 		{
 			for (int j = 0; j < COLS; j++)
 			{
-				array[i][j] = array[i + 1][j + 1];
+				if (j == (COLS - 1) && i != ROWS - 1)
+				{
+					array[i][j] = array[i + 1][0];
+				}
+				else
+				{
+					array[i][j] = array[i][j + 1];
+				}
 			}
 		}
 		array[ROWS - 1][COLS - 1] = buffer;
@@ -715,6 +725,29 @@ void shiftRight(char array[], const int n, int number_of_shifts)
 			array[i] = array[i - 1];
 		}
 		array[0] = buffer;
+	}
+}
+
+void shiftRight(int array[ROWS][COLS], const int ROWS, const int COLS, int number_of_shifts)
+{
+	for (int i = 0; i < number_of_shifts; i++)
+	{
+		int buffer = array[ROWS - 1][COLS - 1];
+		for (int i = ROWS - 1; i > -1; i--)
+		{
+			for (int j = COLS - 1; j > -1; j--)
+			{
+				if (j == 0 && i != 0)
+				{
+					array[i][j] = array[i - 1][COLS - 1];
+				}
+				else
+				{
+					array[i][j] = array[i][j - 1];
+				}
+			}
+		}
+		array[0][0] = buffer;
 	}
 }
 #endif //SHIFTRIGHT
